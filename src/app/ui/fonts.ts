8bb105b33c0d1ae3a42b7bc4ac06193d7dbc3448
katy-paola <kbarbozab@unicartagena.edu.co>
2023-11-04 01:08:02 -0500

    Add font families

--- src/app/ui/fonts.ts
+++ src/app/ui/fonts.ts
@@ -1,4 +1,7 @@
 import { Righteous, Poppins } from 'next/font/google'
 
 export const titleFont = Righteous({ subsets: ['latin'], weight: ['400'] })
-export const bodyFont = Poppins({ subsets: ['latin'], weight: ['400'] })
+export const bodyFont = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '500', '600'],
+})
