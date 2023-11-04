bc57c18fb52ea27929e59721063629551a7c2af3
katy-paola <kbarbozab@unicartagena.edu.co>
2023-11-04 01:05:19 -0500

    Convert SVGs to JSX components

---
+++ src/svg/More.tsx
@@ -0,0 +1,16 @@
+export default function More() {
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M8.00001 14.6666L3.33334 9.99992L4.26668 9.06659L7.33334 12.1166V1.33325H8.66668V12.1166L11.7333 9.04992L12.6667 9.99992L8.00001 14.6666Z"
+        fill="currentColor"
+      />
+    </svg>
+  )
+}
