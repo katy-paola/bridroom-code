bc50857662080ea0a097de32da94d07cbbe582d5
katy-paola <kbarbozab@unicartagena.edu.co>
2024-01-15 18:05:57 -0500

    Remove unused code and refactor HouseSection component

--- src/components/HouseSection.tsx
+++ src/components/HouseSection.tsx
@@ -1,15 +1,11 @@
 import More from '@/svg/More'
 import Button from './Button'
-import Card from './Card'
 import HouseHeader from './HouseHeader'
 import Link from 'next/link'
 import { ListListings, ListListingSkeleton } from './ListListings'
-import { getAllListings } from '@/services/listing'
 import { Suspense } from 'react'
 
 export default async function HouseSection() {
-  const listings = await getAllListings()
-
   return (
     <section className="flex flex-col gap-6 bg-neutral-secondary-bg px-4 py-8 xs:px-8 sm:gap-8 sm:px-12 sm:py-10">
       <HouseHeader />
@@ -17,20 +13,7 @@ export default async function HouseSection() {
         <Suspense fallback={<ListListingSkeleton />}>
           <ListListings />
         </Suspense>
-        <ul className="grid grid-cols-auto-fill grid-rows-auto-fit items-stretch gap-6">
-          {listings?.map((listing) => (
-            <li key={listing.id} className="contents">
-              <Card
-                photo={listing.photos?.[0]}
-                title={listing.title}
-                name={listing.owner?.name}
-                rating={listing.rating}
-                price={listing.price}
-                id={listing.id}
-              />
-            </li>
-          ))}
-        </ul>
+
         <section className="flex justify-end sm:justify-center">
           <Link href="/house">
             <Button
