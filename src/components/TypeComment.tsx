893a4483918210c6ffc77143488b908261b1a70c
katy-paola <kbarbozab@unicartagena.edu.co>
2024-01-15 17:29:15 -0500

    Style Type Comment component

--- src/components/TypeComment.tsx
+++ src/components/TypeComment.tsx
@@ -3,13 +3,21 @@ import Button from './Button'
 
 export default function TypeComment() {
   return (
-    <section>
-      <section>
-        <h5>Dejar un comentario</h5>
+    <section className="flex flex-col items-end gap-4 bg-neutral-secondary-bg p-4">
+      <section className="flex w-full flex-col gap-2">
+        <h5 className="text-paragraph-regular font-medium text-neutral-title">
+          Dejar un comentario
+        </h5>
         <Rating />
-        <textarea className="border"></textarea>
+        <textarea className="h-10 border border-solid border-neutral-paragraph bg-transparent p-2 text-paragraph-small outline-none"></textarea>
       </section>
-      <Button type="secondary" size="small" hasText="yes" text="Publicar" />
+      <Button
+        type="secondary"
+        size="small"
+        hasText="yes"
+        text="Publicar"
+        width="w-max"
+      />
     </section>
   )
 }
