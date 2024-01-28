4bc82f2c2eb4611a9c34b633e6b7c39d801ce522
katy-paola <kbarbozab@unicartagena.edu.co>
2024-01-28 17:06:00 -0500

    Update TypeComment component styling (responsive sm)

--- src/components/TypeComment.tsx
+++ src/components/TypeComment.tsx
@@ -3,16 +3,16 @@ import Button from './Button'
 
 export default function TypeComment() {
   return (
-    <section className="flex flex-col items-end gap-4 bg-neutral-secondary-bg p-4 xs:px-8">
+    <section className="flex flex-col items-end gap-4 bg-neutral-active px-4 py-8 xs:px-8">
       <section className="flex w-full flex-col gap-2">
         <h5 className="text-paragraph-regular font-medium text-neutral-title">
           Dejar un comentario
         </h5>
         <Rating />
-        <textarea className="h-10 border border-solid border-neutral-paragraph bg-transparent p-2 text-paragraph-small outline-none"></textarea>
+        <textarea className="h-10 border border-solid border-neutral-paragraph bg-transparent p-2 text-paragraph-small outline-none sm:h-16"></textarea>
       </section>
       <Button
-        type="secondary"
+        type="cuaternary"
         size="small"
         hasText="yes"
         text="Publicar"
