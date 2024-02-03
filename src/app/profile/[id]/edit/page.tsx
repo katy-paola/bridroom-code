6b367bb94540bfa9539ee6312503a0a2b3552e34
katy-paola <kbarbozab@unicartagena.edu.co>
2024-02-03 17:09:03 -0500

    Refactor profile edit page layout

--- src/app/profile/[id]/edit/page.tsx
+++ src/app/profile/[id]/edit/page.tsx
@@ -6,57 +6,59 @@ import ImgEditProfile from '@/svg/ImgEditProfile'
 export default async function ProfileEdit() {
   const currentUser = await getProfileCurrentUser()
   return (
-    <section className="mt-14 w-full md:mt-20">
-      <h2>Editar perfil</h2>
-      <form>
-        <fieldset>
-          <label>
-            Cambiar foto de perfil
-            <figure>
-              <img src="/upload-photo.svg" alt="" />
-            </figure>
-            <input
-              className="sr-only"
-              type="file"
-              accept="image/*"
-              capture="environment"
-              multiple
-            />
-          </label>
-          <label>
-            Editar descripción:
-            <InputForm
-              type="text"
-              placeholder="Agrega información sobre ti, como tus gustos, hobbies, etc."
-              hasIcon={false}
-              isRadio={false}
-              value={currentUser?.about}
-            />
-          </label>
-          <label>
-            {currentUser?.role === 'student' ? 'Universidad' : 'Barrio'}
-            <InputForm
-              type="text"
-              placeholder={
-                currentUser?.role === 'student'
-                  ? 'Ej.: Universidad de Cartagena'
-                  : 'Ej.: San Fernando'
-              }
-              hasIcon={false}
-              isRadio={false}
-              value={currentUser?.university}
-            />
-          </label>
-        </fieldset>
-        <Button
-          type="primary"
-          size="regular"
-          hasText="yes"
-          text="Guardar cambios"
-          width="w-full"
-        />
-      </form>
-      <ImgEditProfile />
+    <section className="mt-14 flex w-full flex-col items-center gap-8 p-4 xs:p-8 sm:px-44 md:mt-16 md:px-72 lg:flex-row lg:items-center lg:gap-16 lg:px-36 lg:py-10 xl:px-60">
+      <section className="contents w-full flex-col gap-8 lg:flex">
+        <h2 className="text-paragraph-medium font-medium text-neutral-title md:text-paragraph-large">
+          Editar perfil
+        </h2>
+        <form className="flex flex-col gap-8">
+          <fieldset className="flex flex-col gap-8">
+            <label className="flex flex-col gap-2 text-paragraph-regular text-neutral-paragraph">
+              Cambiar foto de perfil
+              <figure>
+                <img src="/upload-photo.svg" alt="" />
+              </figure>
+              <input className="sr-only" type="file" accept="image/*" />
+            </label>
+            <label className="flex flex-col gap-2 text-paragraph-regular text-neutral-paragraph">
+              Editar descripción:
+              <InputForm
+                type="text"
+                placeholder="Agrega información sobre ti, como tus gustos, hobbies, etc."
+                hasIcon={false}
+                isRadio={false}
+                value={currentUser?.about}
+              />
+            </label>
+            <label className="flex flex-col gap-2 text-paragraph-regular text-neutral-paragraph">
+              {currentUser?.role === 'student' ? 'Universidad' : 'Barrio'}
+              <InputForm
+                type="text"
+                placeholder={
+                  currentUser?.role === 'student'
+                    ? 'Ej.: Universidad de Cartagena'
+                    : 'Ej.: San Fernando'
+                }
+                hasIcon={false}
+                isRadio={false}
+                value={currentUser?.university}
+              />
+            </label>
+          </fieldset>
+          <Button
+            type="primary"
+            size="regular"
+            hasText="yes"
+            text="Guardar cambios"
+            width="w-full"
+          />
+        </form>
+      </section>
+      <section className="flex justify-center">
+        <figure className="w-72 md:w-96 lg:w-[480px]">
+          <ImgEditProfile />
+        </figure>
+      </section>
     </section>
   )
 }
