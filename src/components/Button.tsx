77baf4a1ad66463da1df1a1937b02dfc4b848274
Katy Barboza <100061789+katy-paola@users.noreply.github.com>
2024-02-26 16:02:36 +0000

    Refactor Button component styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -26,22 +26,22 @@ export default function Button(Props: {
   } = Props
 
   const setClassName = () => {
-    return `flex ${width} items-center justify-center gap-2 leading-none outline-none ${setTypeClassName()} ${
+    return `flex ${width} box-border items-center justify-center gap-2 leading-none outline-none ${setTypeClassName()} ${
       id === 'view-map' && 'bg-transparent'
     } ${setSizeClassName()} ${type !== 'tab' && 'rounded-lg'} ${setPadding()}`
   }
   const setTypeClassName = () => {
     return type === 'primary'
-      ? 'bg-primary-default text-neutral-main-bg hover:bg-primary-hover active:bg-primary-active disabled:bg-primary-disabled'
+      ? 'bg-primary-default border-solid border-transparent border-2 text-neutral-main-bg hover:bg-primary-hover active:bg-primary-active disabled:bg-primary-disabled'
       : type === 'secondary'
-      ? 'bg-secondary-default text-neutral-main-bg hover:bg-secondary-hover active:bg-secondary-active disabled:bg-secondary-disabled'
+      ? 'bg-secondary-default border-solid border-transparent border-2 text-neutral-main-bg hover:bg-secondary-hover active:bg-secondary-active disabled:bg-secondary-disabled'
       : type === 'tertiary'
       ? id === 'view-map'
         ? 'bg-transparent text-tertiary-default hover:text-neutral-main-bg hover:bg-tertiary-default'
-        : 'bg-tertiary-default text-neutral-main-bg hover:bg-tertiary-hover active:bg-tertiary-active disabled:bg-tertiary-disabled'
+        : 'bg-tertiary-default border-solid border-transparent border-2 text-neutral-main-bg hover:bg-tertiary-hover active:bg-tertiary-active disabled:bg-tertiary-disabled'
       : type === 'cuaternary'
       ? 'bg-transparent border-solid border-secondary-default border-2 text-secondary-default hover:bg-secondary-default hover:text-neutral-main-bg active:bg-secondary-hover active:text-neutral-main-bg active:border-secondary-hover disabled:bg-secondary-disabled disabled:text-neutral-main-bg'
-      : 'bg-transparent text-neutral-title hover:bg-neutral-hover active:bg-neutral-active disabled:text-neutral-placeholder hover:border-l-2 hover:border-l-primary-default hover:border-l-solid md:hover:border-l-0 md:hover:border-b-2 md:hover:border-b-primary-default md:hover:border-b-solid'
+      : 'bg-transparent text-neutral-title border-transparent border-2 border-solid hover:bg-neutral-hover active:bg-neutral-active disabled:text-neutral-placeholder hover:border-l-primary-default md:hover:border-l-transparent md:hover:border-b-primary-default'
   }
   const setSizeClassName = () => {
     return size === 'small'
@@ -74,22 +74,14 @@ export default function Button(Props: {
       onClick={onClick}
       disabled={disabled}
     >
-      {iconLeft !== undefined && <figure
-          className={`${
-            hasText === 'no'
-              ? 'w-6'
-              : 'w-3'
-          }`}
-        >{iconLeft}</figure>}
+      {iconLeft !== undefined && (
+        <figure className={`${hasText === 'no' ? 'w-6' : 'w-3'}`}>
+          {iconLeft}
+        </figure>
+      )}
       <span className={`${setText()} font-semibold`}>{text}</span>
       {iconRight !== undefined && (
-        <figure
-          className={`${
-            hasText === 'no'
-              ? 'w-6'
-              : 'w-3'
-          }`}
-        >
+        <figure className={`${hasText === 'no' ? 'w-6' : 'w-3'}`}>
           {iconRight}
         </figure>
       )}
