e259fb488f0c4773a9645578e4aa3ab6b24206d8
Katy Barboza Baena <kbarbozab@unicartagena.edu.co>
2024-04-17 18:11:12 -0500

    Refactor Comment component to handle empty message

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -32,42 +32,42 @@ export default function Comment(Props: {
             <Rating numberOfStars={rating} />
           </section>
         </section>
-        
-          {message !== null &&
-            <section className="flex flex-col gap-1">
-              <p className="text-paragraph-small font-normal text-neutral-paragraph lg:max-w-prose">
-                {message}
-              </p>
-              <button
-                id={`comment-${id}`}
-                onClick={() => {
-                  setShowInput((prev) => !prev)
-                }}
-                className="self-end text-paragraph-xsmall font-normal text-neutral-paragraph underline"
-              >
-                Responder
-              </button>
-              {showInput && (
-                <section className="flex flex-col items-end gap-2">
-                  <textarea
-                    className="flex h-10 resize-none items-center self-stretch border border-solid border-neutral-paragraph bg-transparent p-2 text-paragraph-small outline-none sm:h-16"
-                    placeholder="Escribe tu respuesta..."
-                    required
-                  ></textarea>
-                  <Button
-                    variant="cuaternary"
-                    size="small"
-                    hasText="yes"
-                    text="Publicar"
-                    width="w-max"
-                    onClick={() => {
-                      setShowInput(false)
-                    }}
-                  />
-                </section>
-              )}
-            </section>
-          }
+
+        {message !== '' && (
+          <section className="flex flex-col gap-1">
+            <p className="text-paragraph-small font-normal text-neutral-paragraph lg:max-w-prose">
+              {message}
+            </p>
+            <button
+              id={`comment-${id}`}
+              onClick={() => {
+                setShowInput((prev) => !prev)
+              }}
+              className="self-end text-paragraph-xsmall font-normal text-neutral-paragraph underline"
+            >
+              Responder
+            </button>
+            {showInput && (
+              <section className="flex flex-col items-end gap-2">
+                <textarea
+                  className="flex h-10 resize-none items-center self-stretch border border-solid border-neutral-paragraph bg-transparent p-2 text-paragraph-small outline-none sm:h-16"
+                  placeholder="Escribe tu respuesta..."
+                  required
+                ></textarea>
+                <Button
+                  variant="cuaternary"
+                  size="small"
+                  hasText="yes"
+                  text="Publicar"
+                  width="w-max"
+                  onClick={() => {
+                    setShowInput(false)
+                  }}
+                />
+              </section>
+            )}
+          </section>
+        )}
       </article>
     </li>
   )
