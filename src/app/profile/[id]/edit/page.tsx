9e150765e78b7398640ba85b4a3641b09c3cc03b
Katy Barboza Baena <kbarbozab@unicartagena.edu.co>
2024-05-03 19:34:27 -0500

    Add profile update functionality - check why it does not work

--- src/app/profile/[id]/edit/page.tsx
+++ src/app/profile/[id]/edit/page.tsx
@@ -1,17 +1,50 @@
 import Button from '@/components/Button'
 import InputForm from '@/components/InputForm'
+import { createClient } from '@/lib/supabase/server'
 import { getProfileCurrentUser } from '@/services/user'
 import ImgEditProfile from '@/svg/ImgEditProfile'
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
 
 export default async function ProfileEdit() {
   const currentUser = await getProfileCurrentUser()
+
+  const updateProfile = async (formData: FormData) => {
+    'use server'
+    console.log('formData', formData)
+    const name = formData.get('name') as string
+    const about = formData.get('about') as string
+    const place = formData.get('place') as string
+    const cookieStore = cookies()
+    const supabase = createClient(cookieStore)
+
+    const user = await supabase.auth.getUser()
+
+    if (user.error !== null) {
+      return redirect(
+        '/profile/edit?message=You must be logged in to edit your profile&error=true',
+      )
+    }
+
+    const { error } = await supabase
+      .from('profiles')
+      .update({ name, about, university: place })
+      .eq('id', user.data.user?.id)
+
+    if (error !== null) {
+      return redirect(`/profile/edit?message=${error.message}&error=true`)
+    }
+
+    return redirect('/profile?message=Profile updated!')
+  }
+
   return (
     <section className="mt-14 flex w-full flex-col gap-8 p-4 xs:p-8 sm:px-44 md:mt-16 md:px-72 lg:min-h-main lg:flex-row lg:items-center lg:gap-16 lg:px-36 lg:py-10 xl:px-60">
       <section className="contents w-full flex-col gap-8 lg:flex">
         <h2 className="text-paragraph-medium font-medium text-neutral-title md:text-paragraph-large">
           Editar perfil
         </h2>
-        <form className="flex flex-col gap-8">
+        <form action={updateProfile} className="flex flex-col gap-8">
           <fieldset className="flex flex-col gap-8">
             <label className="flex flex-col gap-2 text-paragraph-regular text-neutral-paragraph">
               Foto de perfil:
@@ -24,6 +57,7 @@ export default async function ProfileEdit() {
               Nombre:
               <InputForm
                 type="text"
+                name="name"
                 placeholder="Agrega tu nombre."
                 hasIcon={false}
                 isRadio={false}
@@ -34,6 +68,7 @@ export default async function ProfileEdit() {
               Descripción:
               <InputForm
                 type="text"
+                name="about"
                 placeholder="Agrega información sobre ti, como tus gustos, hobbies, etc."
                 hasIcon={false}
                 isRadio={false}
@@ -44,6 +79,7 @@ export default async function ProfileEdit() {
               {currentUser?.role === 'student' ? 'Universidad' : 'Barrio'}
               <InputForm
                 type="text"
+                name="place"
                 placeholder={
                   currentUser?.role === 'student'
                     ? 'Ej.: Universidad de Cartagena.'
