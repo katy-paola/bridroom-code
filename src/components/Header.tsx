d4c739cd64ad566638d4c05dc0494e7dc3c4722d
katy-paola <kbarbozab@unicartagena.edu.co>
2024-05-09 22:14:36 -0500

    chore: Update npm dependency to latest stable version and enable turbo mode in development script

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,16 @@
 import { getProfileCurrentUser, getSession } from '@/services/user'
 import Logo from '@/svg/Logo'
+import { User } from '@/types/types'
 import Link from 'next/link'
 import MenuResponsive from './MenuResponsive'
 
 export default async function Header() {
-  const user = await getProfileCurrentUser()
+  const user = (await getProfileCurrentUser()) as User
   const session = await getSession()
 
   return (
     <header className="fixed z-[1000] flex w-full items-center justify-between bg-neutral-secondary-bg p-4 text-neutral-title shadow-sm xs:px-8 sm:px-12">
-      <Link href="/" className="h-6 md:h-8">
+      <Link href="/" className="size-6 md:size-8">
         <Logo />
       </Link>
       <MenuResponsive session={session} user={user} />
