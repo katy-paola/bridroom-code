fcf5f7714d7de2a0c9d3926a655e86a9639da738
katy-paola <katybarbozabaena@gmail.com>
2024-07-20 17:49:51 -0500

    refactor: Update profile edit page layout and styles

--- src/app/profile/[id]/edit/page.tsx
+++ src/app/profile/[id]/edit/page.tsx
@@ -48,61 +48,66 @@ export default async function ProfileEdit() {
   }
 
   return (
-    <section className="mt-14 flex w-full flex-col gap-8 p-4 xs:p-8 sm:px-44 md:mt-16 md:px-72 lg:min-h-main lg:flex-row lg:items-center lg:gap-16 lg:px-36 lg:py-10 xl:px-60">
-      <section className="contents w-full flex-col gap-8 lg:flex">
-        <h2 className="text-paragraph-medium font-medium text-neutral-title md:text-paragraph-large">
-          Editar perfil
-        </h2>
-        <form action={updateProfile} className="flex flex-col gap-8">
-          <fieldset className="flex flex-col gap-8">
-            <InputFilePreview label="Foto de perfil:" multiple={false} />
-            <label className="flex flex-col gap-2 text-paragraph-regular text-neutral-paragraph">
-              Nombre:
-              <InputForm
-                type="text"
-                name="name"
-                placeholder="Agrega tu nombre."
-                hasIcon={false}
-                isRadio={false}
-                value={currentUser?.about}
-              />
-            </label>
-            <label className="flex flex-col gap-2 text-paragraph-regular text-neutral-paragraph">
-              Descripción:
-              <InputForm
-                type="text"
-                name="about"
-                placeholder="Agrega información sobre ti, como tus gustos, hobbies, etc."
-                hasIcon={false}
-                isRadio={false}
-                value={currentUser?.about}
-              />
-            </label>
-            <label className="flex flex-col gap-2 text-paragraph-regular text-neutral-paragraph">
-              {currentUser?.role === 'student' ? 'Universidad' : 'Barrio'}
-              <InputForm
-                type="text"
-                name="place"
-                placeholder={
-                  currentUser?.role === 'student'
-                    ? 'Ej.: Universidad de Cartagena.'
-                    : 'Ej.: San Fernando.'
-                }
-                hasIcon={false}
-                isRadio={false}
-                value={currentUser?.university}
-              />
-            </label>
-          </fieldset>
-          <Button
-            variant="primary"
-            size="regular"
-            type="submit"
-            hasText="yes"
-            text="Guardar cambios"
-            width="w-full"
-          />
-        </form>
+    <section className="flex min-h-screen w-full flex-col items-center gap-8 bg-neutral-main-bg px-4 pb-8 pt-16 xs:px-8 sm:justify-center sm:px-40 md:px-28 md:pt-22 lg:flex-row lg:gap-16 lg:px-36 lg:pb-20 lg:pt-42 xl:px-60">
+      <section className="contents flex-col gap-8 lg:flex">
+        <section className="flex w-full flex-col gap-4 sm:w-auto">
+          <h2 className="text-paragraph-medium font-medium text-neutral-title md:text-paragraph-large">
+            Editar perfil
+          </h2>
+          <form
+            action={updateProfile}
+            className="flex w-full flex-col gap-8 sm:min-w-96 sm:max-w-sm md:max-w-md"
+          >
+            <fieldset className="flex flex-col gap-8">
+              <InputFilePreview label="Foto de perfil:" multiple={false} />
+              <label className="flex flex-col gap-2 text-paragraph-regular text-neutral-paragraph">
+                Nombre:
+                <InputForm
+                  type="text"
+                  name="name"
+                  placeholder="Agrega tu nombre."
+                  hasIcon={false}
+                  isRadio={false}
+                  value={currentUser?.name}
+                />
+              </label>
+              <label className="flex flex-col gap-2 text-paragraph-regular text-neutral-paragraph">
+                Descripción:
+                <InputForm
+                  type="text"
+                  name="about"
+                  placeholder="Agrega información sobre ti, como tus gustos, hobbies, etc."
+                  hasIcon={false}
+                  isRadio={false}
+                  value={currentUser?.about}
+                />
+              </label>
+              <label className="flex flex-col gap-2 text-paragraph-regular text-neutral-paragraph">
+                {currentUser?.role === 'student' ? 'Universidad' : 'Barrio'}
+                <InputForm
+                  type="text"
+                  name="place"
+                  placeholder={
+                    currentUser?.role === 'student'
+                      ? 'Ej.: Universidad de Cartagena.'
+                      : 'Ej.: San Fernando.'
+                  }
+                  hasIcon={false}
+                  isRadio={false}
+                  value={currentUser?.university}
+                />
+              </label>
+            </fieldset>
+            <Button
+              variant="primary"
+              size="regular"
+              type="submit"
+              hasText="yes"
+              text="Guardar cambios"
+              width="w-full"
+            />
+          </form>
+        </section>
       </section>
       <section className="flex justify-center">
         <figure className="w-72 md:w-96 lg:w-[480px]">
