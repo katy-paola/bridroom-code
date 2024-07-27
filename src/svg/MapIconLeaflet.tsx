0d073c9849a63eaab981489dadf3da7acbe1f31d
katy-paola <katybarbozabaena@gmail.com>
2024-07-27 18:53:54 -0500

    refactor: Update MapIconLeaflet component to adjust icon and popup anchor positions

--- src/svg/MapIconLeaflet.tsx
+++ src/svg/MapIconLeaflet.tsx
@@ -5,6 +5,6 @@ import { Icon } from 'leaflet'
 export const MapIconLeaflet = new Icon({
   iconUrl: 'icons/map-marker.svg',
   iconSize: [36, 36],
-  iconAnchor: [36, 36],
-  popupAnchor: [-3, -76],
+  iconAnchor: [18, 36],
+  popupAnchor: [0, -36],
 })
