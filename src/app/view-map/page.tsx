a9742e340c87d784f492562dcc65d7eca9df7c25
katy-paola <katybarbozabaena@gmail.com>
2024-07-29 20:15:21 -0500

    refactor: Add session check and redirect to login page in ViewMap page

--- src/app/view-map/page.tsx
+++ src/app/view-map/page.tsx
@@ -1,9 +1,16 @@
 import { getAllListings } from '@/services/listing'
-import { getProfileCurrentUser } from '@/services/user'
+import { getProfileCurrentUser, getSession } from '@/services/user'
+import { redirect } from 'next/navigation'
 import { MapLeaflet } from './map'
 
 export default async function ViewMap() {
   const currentUser = await getProfileCurrentUser()
+  const session = await getSession()
+
+  if (session === null) {
+    return redirect('/login')
+  }
+
   const role = currentUser?.role
   const listings = await getAllListings({
     role,
